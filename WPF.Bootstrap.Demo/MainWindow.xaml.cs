19bcafbf9383978a0fdf12ee3859f8c9de9f34b8
何山 <5373827@qq.com>
2015-07-15 17:37:39 +0800

    add dialog style

--- WPF.Bootstrap.Demo/MainWindow.xaml.cs
+++ WPF.Bootstrap.Demo/MainWindow.xaml.cs
@@ -12,13 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF.Bootstrap.Controls;
 
 namespace WPF.Bootstrap.Demo
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
